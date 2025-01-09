efc0f89bdcac04fb73e57a6a51262369253d0eb2
Dmitry <dmitryfil@Mac-mini-M4.local>
2025-01-09 21:52:05 +0300

    upd7

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
+    }
+  },
+
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   // css: ['~/assets/css/main.css'],
@@ -18,3 +25,8 @@ export default defineNuxtConfig({
     // autoImport: true
   }
 })
+
+
+
+
+
